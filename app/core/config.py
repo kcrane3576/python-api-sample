3fadb9b351c7341bf01cfababbb25c2e18abba28
Kyle <kcrane121@gmail.com>
2025-07-09 11:25:43 -0400

    Configure environment variables with basic test config (#2)
    * updated default branching setup in README
    
    * setup environment config with basic tests

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Read which file to load
+env_file = os.getenv("ENV_FILE", ".env")
+
+print(env_file)
+
+# Resolve path relative to project root
+env_path = Path(__file__).resolve().parent.parent.parent / env_file
+print(env_path)
+load_dotenv(dotenv_path=env_path)
+
+class Settings:
+    ENV: str = os.getenv("ENV", "development")
+
+settings = Settings()
