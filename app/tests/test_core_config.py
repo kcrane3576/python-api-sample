107c65ffe0992a1cfb37fbf8cc783864e568faff
kcrane <kcrane121@gmail.com>
2025-07-11 14:30:55 -0400

    debug to get env file

--- app/tests/test_core_config.py
+++ app/tests/test_core_config.py
@@ -1,5 +1,7 @@
+import os
 from core import config
 
 def test_env_config_loads():
-    print(f"config.settings.ENV: ${config.settings.ENV}")
+    print(f"ENV_FILE: {os.getenv('ENV_FILE')}")
+    print(f"Actual ENV loaded: {config.settings.ENV}")
     assert config.settings.ENV == "test"
