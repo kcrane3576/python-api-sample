45db83ca724ef769449c12dbd4cb562f700eb0f1
kcrane <kcrane121@gmail.com>
2025-07-12 19:20:34 -0400

    removed unused logging config

--- app/services/elasticsearch.py
+++ app/services/elasticsearch.py
@@ -1,9 +1,6 @@
-import logging
 import time
 from elasticsearch import Elasticsearch
 from core.config import settings
-
-logger = logging.getLogger(__name__)
 
 def get_elasticsearch_client():
     return Elasticsearch(hosts=[settings.ELASTICSEARCH_URL])
